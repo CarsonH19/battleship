2df578dae5d8f94bb079a7b1e8a45ed10bdc2532
CarsonH19 <questline.carson@gmail.com>
2024-02-21 21:41:09 +0900

    feat: GameBoard class, createGrid, & placeShip

--- src/index.js
+++ src/index.js
@@ -1,20 +1,49 @@
 // import "./style.css";
 
 export class Ship {
-  constructor (length) {
+  constructor(length) {
     this.length = length;
     this.hits = 0;
     this.sunk = false;
   }
 
-  hit () {
+  hit() {
     this.hits++;
     if (this.hits === this.length) {
       this.sunk = true;
     }
   }
 
-  isSunk () {
+  isSunk() {
     return this.sunk;
+  }
+}
+
+export class GameBoard {
+  constructor(gridSize = 10) {
+    this.gridSize = gridSize;
+    this.grid = this.createGrid();
+    this.ships = [];
+    this.missedAttacks = [];
+  }
+
+  createGrid() {
+    return (
+      Array.from({ length: this.gridSize }),
+      () => Array(this.gridSize).fill(null)
+    );
+  }
+
+  placeShip (ship, x, y, orientation) {
+    if (orientation === 'horizontal') {
+      for (let i = 0; i < ship.length; i++) {
+        this.grid[x + i] = ship;
+      }
+    } else {
+      for (let i = 0; i < ship.length; i++) {
+        this.grid[x][y + i] = ship;
+      }
+    }
+    this.ships.push({ ship, x, y, orientation });
   }
 }
